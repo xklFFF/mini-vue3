9b72202651597f564d6e9df6b5e8a2095c626e8e
xukailin <765111075@qq.com>
2023-04-25 19:48:35 +0800

    实现解析element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,7 +1,31 @@
 import { NodeTypes } from "./ast";
+const enum TagType {
+    Start,
+    End,
+}
 export function baseParse(content) {
     const context = createParserContext(content)
     return createRoot(parseChildren(context))
+}
+function parseElement(context) {
+    const element = parseTag(context, TagType.Start)
+    parseTag(context, TagType.End)
+    return element
+
+}
+function parseTag(context, type) {
+    // <div></div>
+    const match: any = /^<\/?([a-z]*)/i.exec(context.source);
+    const tag = match[1];
+    advanceBy(context, match[0].length);
+    advanceBy(context, 1);
+
+    if (type === TagType.End) return;
+
+    return {
+        type: NodeTypes.ELEMENT,
+        tag,
+    };
 }
 function parseInterpolation(context) {
     // {{message}}
@@ -50,8 +74,13 @@ function parseChildren(context) {
     const nodes: any = [];
 
     let node;
-    if (context.source.startsWith("{{")) {
+    const s = context.source
+    if (s.startsWith("{{")) {
         node = parseInterpolation(context);
+    } else if (s[0] === "<") {
+        if (/[a-z]/i.test(s[1])) {
+            node = parseElement(context);
+        }
     }
 
     nodes.push(node);
