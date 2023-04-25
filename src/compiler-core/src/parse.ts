3a30a6394d2cfd4de54d07afa5b6af6a595b99c6
xukailin <765111075@qq.com>
2023-04-25 21:34:05 +0800

    实现解析联合类型、嵌套、标签不闭合

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -5,13 +5,20 @@ const enum TagType {
 }
 export function baseParse(content) {
     const context = createParserContext(content)
-    return createRoot(parseChildren(context))
+    return createRoot(parseChildren(context, []))
 }
-function parseElement(context) {
-    const element = parseTag(context, TagType.Start)
-    parseTag(context, TagType.End)
-    return element
+function parseElement(context: any, ancestors) {
+    const element: any = parseTag(context, TagType.Start);
+    ancestors.push(element);
+    element.children = parseChildren(context, ancestors);
+    ancestors.pop();
+    if (startsWithEndTagOpen(context.source, element.tag)) {
+        parseTag(context, TagType.End);
+    } else {
+        throw new Error(`缺少结束标签:${element.tag}`);
+    }
 
+    return element;
 }
 function parseTag(context, type) {
     // <div></div>
@@ -45,7 +52,7 @@ function parseInterpolation(context) {
     const rawContent = parseTextData(context, rawContentLength);
     const content = rawContent.trim()
 
-    advanceBy(context, rawContentLength + closeDelimiter.length);
+    advanceBy(context, closeDelimiter.length);
 
     return {
         type: NodeTypes.INTERPOLATION,
@@ -56,7 +63,17 @@ function parseInterpolation(context) {
     };
 }
 function parseText(context: any) {
-    const content = parseTextData(context, context.source.length)
+    let endIndex = context.source.length;
+    let endTokens = ["<", "{{"];
+    //找到离得最近的结束标签
+    for (let i = 0; i < endTokens.length; i++) {
+        const index = context.source.indexOf(endTokens[i]);
+        if (index !== -1 && endIndex > index) {
+            endIndex = index;
+        }
+    }
+
+    const content = parseTextData(context, endIndex);
     return {
         type: NodeTypes.TEXT,
         content
@@ -85,20 +102,39 @@ function createRoot(children) {
         children,
     };
 }
-function parseChildren(context) {
+function parseChildren(context, ancestors) {
     const nodes: any = [];
-
-    let node;
-    const s = context.source
-    if (s.startsWith("{{")) {
-        node = parseInterpolation(context);
-    } else if (s[0] === "<") {
-        if (/[a-z]/i.test(s[1])) {
-            node = parseElement(context);
+    while (!isEnd(context, ancestors)) {
+        let node: any
+        const s = context.source
+        if (s.startsWith("{{")) {
+            node = parseInterpolation(context);
+        } else if (s[0] === "<") {
+            if (/[a-z]/i.test(s[1])) {
+                node = parseElement(context, ancestors);
+            }
+        }
+        if (!node) node = parseText(context)
+        nodes.push(node);
+    }
+    return nodes;
+}
+function isEnd(context, ancestors) {
+    const s = context.source;
+    if (s.startsWith("</")) {
+        for (let i = ancestors.length - 1; i >= 0; i--) {
+            const tag = ancestors[i].tag;
+            if (startsWithEndTagOpen(s, tag)) {
+                return true;
+            }
         }
     }
-    if (!node) node = parseText(context)
-    nodes.push(node);
+    return !s;
+}
 
-    return nodes;
+function startsWithEndTagOpen(source, tag) {
+    return (
+        source.startsWith("</") &&
+        source.slice(2, 2 + tag.length).toLowerCase() === tag.toLowerCase()
+    );
 }
