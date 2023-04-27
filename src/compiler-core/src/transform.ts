ed62723892b31470b5c614f885d7af11f730aaa1
xukailin <765111075@qq.com>
2023-04-27 12:02:47 +0800

    实现基础的transform功能

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,29 @@
+export function transform(root, options) {
+    const context = createTransformContext(root, options)
+    traverseNode(root, context)
+}
+function createTransformContext(root: any, options: any): any {
+    const context = {
+        root,
+        nodeTransforms: options.nodeTransforms || [],
+    };
+
+    return context;
+}
+//深度遍历
+function traverseNode(node: any, context) {
+    const nodeTransforms = context.nodeTransforms
+    for (const transform of nodeTransforms) {
+        transform(node)
+    }
+    traverseChildren(node, context)
+}
+function traverseChildren(node: any, context: any) {
+    const children = node.children
+    if (children) {
+        for (const node of children) {
+            traverseNode(node, context)
+        }
+    }
+}
+
