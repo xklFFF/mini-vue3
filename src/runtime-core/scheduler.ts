597c8e11d2e85eafd8e6fecd5c032fb81c17a255
xukailin <765111075@qq.com>
2023-05-06 19:41:14 +0800

    完善 scheduler

--- src/runtime-core/scheduler.ts
+++ src/runtime-core/scheduler.ts
@@ -1,10 +1,12 @@
 const queue: any[] = []
+const activePreFlushCbs: any[] = [];
+const activePostFlushCbs:any[] = []
 let p = Promise.resolve()
 
-export function nextTick(fn) {
+export function nextTick(fn?) {
     return fn ? p.then(fn) : p
 }
-export function queueJobs(job) {
+export function queueJob(job) {
     if (!queue.includes(job)) {
         queue.push(job)
     }
@@ -18,11 +20,35 @@ function queueFlush() {
     isPending = true
     nextTick(flushJobs);
 }
-function flushJobs() {
+export function flushJobs() {
 
     isPending = false
+    flushPreFlushCbs()
     let job;
     while ((job = queue.shift())) {
         job && job();
     }
+    flushPostFlushCbs()
 }
+  
+export function queuePreFlushCb(job) {
+    activePreFlushCbs.push(job);
+  
+    queueFlush();
+  }
+export function flushPreFlushCbs() {
+    for (let i = 0; i < activePreFlushCbs.length; i++) {
+        activePreFlushCbs[i]&&activePreFlushCbs[i]();
+    }
+    activePreFlushCbs.length = 0
+  }
+  export function queuePostFlushCb(job){
+    activePostFlushCbs.push(job);
+    queueFlush();
+}
+export function flushPostFlushCbs() {
+    for (let i = 0; i < activePostFlushCbs.length; i++) {
+        activePostFlushCbs[i]&&activePostFlushCbs[i]();
+    }
+    activePostFlushCbs.length = 0
+  }
---
+++ src/runtime-core/test/scheduler.spec.ts
@@ -0,0 +1,92 @@
+import {
+  queueJob,
+  nextTick,
+  queuePostFlushCb,
+  queuePreFlushCb,
+} from '../scheduler'
+describe('scheduler', () => {
+  it('nextTick', async () => {
+    const calls: string[] = []
+    const dummyThen = Promise.resolve().then()
+    const job1 = () => {
+      calls.push('job1')
+    }
+    const job2 = () => {
+      calls.push('job2')
+    }
+    nextTick(job1)
+    job2()
+
+    expect(calls.length).toBe(1)
+    await dummyThen
+    // job1 will be pushed in nextTick
+    expect(calls.length).toBe(2)
+    expect(calls).toMatchObject(['job2', 'job1'])
+  })
+  describe('queueJob', () => {
+    it('basic usage', async () => {
+      const calls: string[] = []
+      const job1 = () => {
+        calls.push('job1')
+      }
+      const job2 = () => {
+        calls.push('job2')
+      }
+      queueJob(job1)
+      queueJob(job2)
+      expect(calls).toEqual([])
+      await nextTick()
+      expect(calls).toEqual(['job1', 'job2'])
+    })
+  })
+  describe('queuePostFlushCb', () => {
+    it('basic usage', async () => {
+      const calls: string[] = []
+      const cb1 = () => {
+        calls.push('cb1')
+      }
+      const cb2 = () => {
+        calls.push('cb2')
+      }
+      const cb3 = () => {
+        calls.push('cb3')
+      }
+
+
+      queuePostFlushCb(cb1)
+      queuePostFlushCb(cb2)
+      queuePostFlushCb(cb3)
+
+      expect(calls).toEqual([])
+      await nextTick()
+      expect(calls).toEqual(['cb1', 'cb2', 'cb3'])
+    })
+  })
+  describe('flush in order', () => {
+    it('basic usage', async () => {
+      const calls: string[] = []
+      const cb1 = () => {
+        calls.push('cb1')
+      }
+      const cb2 = () => {
+        calls.push('cb2')
+      }
+      const cb3 = () => {
+        calls.push('cb3')
+      }
+
+
+      queuePostFlushCb(cb3)
+      queueJob(cb2)
+      queuePreFlushCb(cb1)
+
+      expect(calls).toEqual([])
+      await nextTick()
+      expect(calls).toEqual(['cb1', 'cb2', 'cb3'])
+    })
+  })
+
+})
+
+
+
